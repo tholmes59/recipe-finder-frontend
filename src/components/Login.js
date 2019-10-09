23690a4442de95ca07bcff05a97c998d4ea8d349
Thomas <tholmes59@gmail.com>
2019-10-09 09:11:21 -0700

    update login component to only include username and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,12 +34,12 @@ class Login extends React.Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Name:</label>
-                    <input type="text" placeholder="Enter Name..." value={this.state.name} name="name" onChange={this.handleChange} /><br/>
+                    {/* <label>Name:</label>
+                    <input type="text" placeholder="Enter Name..." value={this.state.name} name="name" onChange={this.handleChange} /><br/> */}
                     <label>Username:</label>
                     <input type="text" placeholder="Enter Username..." value={this.state.username} name="username" onChange={this.handleChange} /><br/>
-                    <label>Email:</label>
-                    <input type="email" placeholder="Enter Email..." value={this.state.email} name="email" onChange={this.handleChange} /><br/>
+                    {/* <label>Email:</label>
+                    <input type="email" placeholder="Enter Email..." value={this.state.email} name="email" onChange={this.handleChange} /><br/> */}
                     <label>Password:</label>
                     <input type="password" placeholder="Enter Password..." value={this.state.password} name="password" onChange={this.handleChange} /><br/>
                     <input type="submit" />
