a50eb613072097db416c5355f744918588b78975
Thomas <tholmes59@gmail.com>
2019-10-14 17:04:57 -0700

    change RecipeCard to class component and add handleClick to fetch recipe details

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import { fetchRecipe } from '../actions/recipeSearch'
+import { connect } from 'react-redux'
 
-const RecipeCard = props => {
+class RecipeCard extends React.Component {
 
-    return (
-        <div>
-            <img src={props.recipe.image_url} alt="Food pic" />
-            <p>{props.recipe.title}</p>
-            <p>Recipe provided by {props.recipe.publisher}</p>
-            <button type="button">Recipe Details</button>
-        </div>
-    )
+    handleClick = (event) => {
+        event.preventDefault();
+        this.props.fetchRecipe(this.props.recipe.recipe_id)
+    }
+    render () {
+        return (
+            <>
+            <div>
+                <img src={this.props.recipe.image_url} alt="Food pic" />
+                <p>{this.props.recipe.title}</p>
+                <p>Recipe provided by {this.props.recipe.publisher}</p>
+                <button type="button" onClick={this.handleClick}>Recipe Details</button>
+            </div>
+            </>
+        )
+    }
 }
 
-export default RecipeCard;
+export default connect(null, {fetchRecipe})(RecipeCard);
