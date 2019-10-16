4c47cfb781ebe3aa4f78a521111b41e06e5056e2
Thomas <tholmes59@gmail.com>
2019-10-16 10:03:48 -0700

    add signup reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import login from './login'
 import currentUser from './currentUser';
 import recipeSearch from './recipeSearch';
+import signup from './signup'
 
 const rootReducer = combineReducers({
     login: login,
     currentUser: currentUser,
-    recipeSearch: recipeSearch
+    recipeSearch: recipeSearch,
+    signup: signup
 })
 
 export default rootReducer
