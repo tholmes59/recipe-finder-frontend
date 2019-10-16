cdd2cbb18c301b6cef39a7616684001db9f1be7b
Thomas <tholmes59@gmail.com>
2019-10-16 11:34:37 -0700

    continue building favorites actions

--- src/actions/favorites.js
+++ src/actions/favorites.js
@@ -38,3 +38,30 @@ export const getFavorites = () => {
         .catch(console.log)
     }
 }
+
+export const addRecipeToFavorits = (recipe) => {
+    return dispatch => {
+        const recipeInfo = {
+            title: recipe.title,
+            recipe_identifier: recipe.recipe_identifier
+        }
+        return fetch("http://loaclhost:3001/api/v1/recipes", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(recipeInfo)
+        })
+        .then(r => r.json())
+        .then(r => {
+            if (r.error) {
+                alert(r.error)
+            } else {
+                dispatchEvent(getFavorites())
+                alert('Saved to Favorites!')
+            }
+        })
+        .catch(console.log)
+    }
+}
