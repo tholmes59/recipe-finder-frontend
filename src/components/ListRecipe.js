899297edc3b7b9c8ca74de85627520e151191119
Thomas <tholmes59@gmail.com>
2019-10-16 09:37:18 -0700

    continue to build out individual recipe component

--- src/components/ListRecipe.js
+++ src/components/ListRecipe.js
@@ -13,8 +13,17 @@ class ListRecipe extends React.Component {
         // console.log(this.props.recipeItems.recipe)
         return (
             <div>
-                Hello from Reicpe!
-               {this.props.recipeItems && this.props.recipeItems.title}
+               <h1>{this.props.recipeItems && this.props.recipeItems.title}</h1>
+               <img src={this.props.recipeItems && this.props.recipeItems.image_url} alt="Recipe" />
+               <h3>Ingrediants</h3>
+               {this.props.recipeItems && this.props.recipeItems.ingredients.map((item, index) => {
+                   return (
+                   <ul key={index}>
+                       {item}
+                   </ul>
+                   )
+               })}
+               <a href={this.props.recipeItems && this.props.recipeItems.source_url} target="_blank" rel="noopener noreferrer">Full Recipe Here</a>
             </div>
         )
     }
