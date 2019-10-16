814c625ff615fd7aa8b0df07a21bbf4338277397
Thomas <tholmes59@gmail.com>
2019-10-16 09:38:11 -0700

    continue to add components to App.js to render to DOM

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login'
+import Logout from './components/Logout'
+import { getCurrentUser } from "./actions/currentUser.js"
+import { connect } from 'react-redux'
+import Signup from './components/Signup';
+import Search from './components/Search'
+import ListRecipe from './components/ListRecipe';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()  
+}
+
   render() {
     return (
       <div className="App">
-       App
+       {this.props.currentUser ? <Logout/> : <Login/>}
+       <Signup />
+       <Search />
+       <ListRecipe />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
