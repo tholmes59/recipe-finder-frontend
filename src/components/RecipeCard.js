8d7f4c785b3a19162bf73404efeca61b169e0064
Thomas <tholmes59@gmail.com>
2019-10-23 11:02:17 -0700

    update recipe card component to the Spoonacular API parameters

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -7,17 +7,17 @@ class RecipeCard extends React.Component {
 
     handleClick = (event) => {
         event.preventDefault();
-        this.props.fetchRecipe(this.props.recipe.recipe_id)
+        this.props.fetchRecipe(this.props.recipe.id)
     }
-    
+
     render () {
         return (
             <>
             <div className="recipeCard">
-                <img src={this.props.recipe.image_url} alt="Food pic" className="recipeCard-img"/>
+                <img src={this.props.recipe.image} alt="Food pic" className="recipeCard-img"/>
                 <p>{this.props.recipe.title}</p>
-                <p>Recipe provided by {this.props.recipe.publisher}</p>
-                <button type="button" onClick={this.handleClick} className="recipeCard-button"><Link to={`/recipe/${this.props.recipe.recipe_id}`}>Recipe Details</Link></button>
+                {/* <p>Recipe provided by {this.props.recipe.publisher}</p> */}
+                <button type="button" onClick={this.handleClick} className="recipeCard-button"><Link to={`/recipe/${this.props.recipe.id}`}>Recipe Details</Link></button>
             </div>
             </>
         )
