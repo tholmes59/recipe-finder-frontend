0921761db0b2f67c63c78bef7edd001a0c0e3fbd
Thomas <tholmes59@gmail.com>
2019-10-25 11:08:32 -0700

    change format of favorites card

--- src/components/FavoritesCard.js
+++ src/components/FavoritesCard.js
@@ -11,10 +11,10 @@ const FavoritesCard = props => {
     }
 
     return (
-        <div>
-            <p>{props.favorite.attributes.recipe_title}</p>
-            <img src={props.favorite.attributes.recipe_image} alt="Food pic" className="recipeCard-img"/>
-            <button type="button" onClick={handleClick}><Link to={`/recipe/${props.favorite.attributes.recipe_identifier}`}>View Recipe</Link></button>
+        <div className="favoritesCard">
+            <img src={props.favorite.attributes.recipe_image} alt="Food pic" className="favoritesCard-img"/><br/><br/>
+            <p>{props.favorite.attributes.recipe_title}</p><br/>
+            <button type="button" onClick={handleClick} className="favoritesCard-button"><Link className="favorites-linkbutton" to={`/recipe/${props.favorite.attributes.recipe_identifier}`}>View Recipe</Link></button>
         </div>
     )
 }
