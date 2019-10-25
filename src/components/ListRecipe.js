795ea1960edcde57c86b75704e489215a3d44ec1
Thomas <tholmes59@gmail.com>
2019-10-25 11:10:58 -0700

    change format and add steps to list recipe page

--- src/components/ListRecipe.js
+++ src/components/ListRecipe.js
@@ -5,11 +5,6 @@ import { addRecipeToFavorites } from '../actions/favorites'
 
 class ListRecipe extends React.Component {
 
-    // componentDidMount() {
-    //     this.props.fetchRecipe(this.props.recipeItems && this.props.recipeItems.recipe_id);
-    //     fetchRecipe(this.props.match.params.id)
-    // }
-
     handleClick = event => {
         event.preventDefault()
         this.props.addRecipeToFavorites(this.props.recipeItems)
@@ -17,20 +12,40 @@ class ListRecipe extends React.Component {
 
     render() {
         return (
-            <div>
-               <h1>{this.props.recipeItems && this.props.recipeItems.title}</h1>
-               <img src={this.props.recipeItems && this.props.recipeItems.image} alt="Recipe" />
-               <h3>Ingredients</h3>
-               {this.props.recipeItems && this.props.recipeItems.extendedIngredients.map((item, index) => {
-                   console.log(item.original)
-                   return (
-                   <ul key={index}>
-                       {item.original}
-                   </ul>
-                   )
-               })}
-               <a href={this.props.recipeItems && this.props.recipeItems.sourceUrl} target="_blank" rel="noopener noreferrer">Full Recipe Here</a><br/>
-               <button onClick={this.handleClick}>Add to Favorites</button>
+            <div className="recipe-container">
+                <div className="recipe-image-container">
+                    <img src={this.props.recipeItems && this.props.recipeItems.image} alt="Recipe" className="recipe-img"/> 
+                </div>
+                <div className="recipe-info-container">
+                    <h1>{this.props.recipeItems && this.props.recipeItems.title}</h1>
+                    <button className="recipe-button"><a className="recipe-linkbutton" href={this.props.recipeItems && this.props.recipeItems.sourceUrl} target="_blank" rel="noopener noreferrer">Full Recipe Here</a></button><br/>
+                    <button className="recipe-button" onClick={this.handleClick}>Add to Favorites</button>
+                </div>
+                <div className="recipe-ingredients-container">
+                    <h3>Ingredients</h3>
+                    {this.props.recipeItems && this.props.recipeItems.extendedIngredients.map((item, index) => {
+                        return (
+                        <ul key={index}>
+                            {item.original}
+                        </ul>
+                        )
+                    })}
+                </div>
+                <div className="recipe-instructions-container">
+                    <h3>Instructions</h3>
+                    {this.props.recipeItems && this.props.recipeItems.analyzedInstructions.map((item) => {
+                        return (
+                            item.steps.map((newItem, index) => {
+                                return (
+                                <ul key={index}>
+                                    <h3>Step {newItem.number}:</h3>
+                                    <p>{newItem.step}</p>
+                                </ul>
+                                )
+                            })
+                        )
+                    })}
+                </div>
             </div>
         )
     }
