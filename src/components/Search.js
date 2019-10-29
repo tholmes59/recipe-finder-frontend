a77ba7d0e7638fbd240dbb8de45f6f40b8ea3a00
Thomas <tholmes59@gmail.com>
2019-10-29 11:46:59 -0700

    edit format of search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -31,7 +31,7 @@ class Search extends React.Component {
                 <h1>Search for a recipe using <span>Spoonacular API</span></h1>
                 <h3>Are you hungry but don't know what to make?</h3>
                 <h4>Type in an ingredient you have in your kitchen below and let Recipe Finder find you the perfect recipe!</h4>
-                <p>For multiple ingredients separate by a comma</p>     
+                <p>For multiple ingredients please separate by a comma</p>     
                 <form onSubmit={this.handleSubmit} className="search-form-container">
                     <input type="text" value={this.state.search} name="search" onChange={this.handleChange} placeholder="chicken,thigh" className="search-input"/>
                     <input value="Search" type="submit" className="search-button"/>
