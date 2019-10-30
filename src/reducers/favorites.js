37102aa9edc28cb7cac8d70b8761c5495059d181
Thomas <tholmes59@gmail.com>
2019-10-30 15:18:00 -0700

    add delete action to favorites reducer

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -4,6 +4,8 @@ export default (state = [], action) => {
             return action.favorites
         case "ADD_FAVORITE":
             return action.favorite
+        case "DELETE_FAVORITE":
+            return state.filter(favorite => favorite.id === action.favoriteId ? false : true)
         case "CLEAR_FAVORITES":
             return []
         default:
