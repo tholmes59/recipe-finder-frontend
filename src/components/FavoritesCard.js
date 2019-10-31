4fa88e4b0a74da76131aea728df3db8b0e2d4911
Thomas <tholmes59@gmail.com>
2019-10-31 11:06:52 -0700

    add delete button to favorites

--- src/components/FavoritesCard.js
+++ src/components/FavoritesCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchRecipe } from '../actions/recipeSearch'
+import { deleteFavorite} from '../actions/favorites'
 import { Link } from 'react-router-dom'
 
 const FavoritesCard = props => {
@@ -10,13 +11,19 @@ const FavoritesCard = props => {
         props.fetchRecipe(props.favorite.attributes.recipe_identifier)
     }
 
+    const handleDelete = event => {
+        event.preventDefault()
+        props.deleteFavorite(props.favorite.id, props.history)
+    }
+
     return (
         <div className="favoritesCard">
             <img src={props.favorite.attributes.recipe_image} alt="Food pic" className="favoritesCard-img"/><br/><br/>
             <p>{props.favorite.attributes.recipe_title}</p><br/>
-            <button type="button" onClick={handleClick} className="favoritesCard-button"><Link className="favorites-linkbutton" to={`/recipe/${props.favorite.attributes.recipe_identifier}`}>View Recipe</Link></button>
+            <button type="button" onClick={handleClick} className="favoritesCard-button"><Link className="favorites-linkbutton" to={`/recipe/${props.favorite.attributes.recipe_identifier}`}>View Recipe</Link></button><br/>
+            <button type="button" onClick={handleDelete} className="favoritesCard-button">Remove from Favorite</button>
         </div>
     )
 }
 
-export default connect(null, { fetchRecipe })(FavoritesCard);
+export default connect(null, { fetchRecipe, deleteFavorite })(FavoritesCard);
