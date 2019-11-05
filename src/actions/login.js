8c8e1855880529234a91c39e10f2673e7befcb70
Thomas <tholmes59@gmail.com>
2019-11-05 11:31:51 -0800

    clean up code

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,10 +1,3 @@
-export const updateLogin = (formData) => {
-    return {
-      type: "UPDATE_LOGIN_FORM",
-      formData
-    }
-  }
-  
   export const resetLogin = () => {
     return {
       type: "RESET_LOGIN_FORM"
