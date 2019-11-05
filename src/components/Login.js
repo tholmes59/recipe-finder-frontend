135dc6ef1ca4330fd53df4f2c530b83cea6e3237
Thomas <tholmes59@gmail.com>
2019-11-05 11:27:37 -0800

    clean up code

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux'
 import { login } from '../actions/currentUser.js'
 import { Link } from 'react-router-dom'
 
-
-
 class Login extends React.Component {
 
     state = {
@@ -37,9 +35,7 @@ class Login extends React.Component {
                 <div className="login-form">
                     <form onSubmit={this.handleSubmit} >
                     <h2 className="login-header">Login</h2>
-                        {/* <label>Username:</label> */}
                         <input type="text" className="login-user" placeholder="Enter Username..." value={this.state.username} name="username" onChange={this.handleChange} /><br/>
-                        {/* <label>Password:</label> */}
                         <input type="password" className="login-pass" placeholder="Enter Password..." value={this.state.password} name="password" onChange={this.handleChange} /><br/>
                         <input type="submit" className="login-button" />
                     </form>
