bdcbc8047df36c8f52b575c0c95844ee958e5487
Thomas <tholmes59@gmail.com>
2019-11-05 11:25:44 -0800

    clean up code

--- src/reducers/signup.js
+++ src/reducers/signup.js
@@ -1,7 +1,5 @@
 export default (state=[], action) => {
     switch (action.type) {
-      case "UPDATE_SIGNUP_FORM":
-        return action.formData
       case "RESET_SIGNUP_FORM":
         return []
       default:
