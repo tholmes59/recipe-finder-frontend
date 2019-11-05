68b38885456fc2af90a2b0a0941c8f23e795ba7a
Thomas <tholmes59@gmail.com>
2019-11-05 11:25:19 -0800

    clean up code

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,7 +1,5 @@
 export default (state = [], action) => {
     switch (action.type) {
-      case "UPDATE_LOGIN_FORM":
-        return action.formData
       case "RESET_LOGIN_FORM":
         return []
       default:
