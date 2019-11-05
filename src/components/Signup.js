7a2f1f258abcb2f449e63ace2633e5a9cd2c4279
Thomas <tholmes59@gmail.com>
2019-11-05 11:27:58 -0800

    clean up code

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux'
 import { signup } from '../actions/currentUser.js'
 import { Link } from 'react-router-dom'
 
-
-
 class Signup extends React.Component {
 
     state = {
@@ -37,20 +35,15 @@ class Signup extends React.Component {
                 <div className="signup-form">
                     <form onSubmit={this.handleSubmit} >
                     <h2 className="signup-header">Sign up</h2>
-                        {/* <label>Name:</label> */}
                         <input type="text" className="signup-name" placeholder="Enter Name..." value={this.state.name} name="name" onChange={this.handleChange} /><br/>
-                        {/* <label>Username:</label> */}
                         <input type="text" className="signup-user" placeholder="Enter Username..." value={this.state.username} name="username" onChange={this.handleChange} /><br/>
-                        {/* <label>Email:</label> */}
                         <input type="email" className="signup-email" placeholder="Enter Email..." value={this.state.email} name="email" onChange={this.handleChange} /><br/>
-                        {/* <label>Password:</label> */}
                         <input type="password" className="signup-pass" placeholder="Enter Password..." value={this.state.password} name="password" onChange={this.handleChange} /><br/>
                         <input type="submit" className="signup-button" value="Sign Up"/>
                     </form>
                     <p>Already a member?</p>
                     <button type="button" className="signup-login-button"><Link className="linkbutton" to={`/login`}>Please Login</Link></button>
                 </div>
-               
             </div>
         )
     }
