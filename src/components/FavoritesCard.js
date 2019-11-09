02073d93f515993ab8b060a1f7c599b6f14f5dbf
Thomas <tholmes59@gmail.com>
2019-11-09 09:43:12 -0800

    convert to class component

--- src/components/FavoritesCard.js
+++ src/components/FavoritesCard.js
@@ -4,26 +4,29 @@ import { fetchRecipe } from '../actions/recipeSearch'
 import { deleteFavorite} from '../actions/favorites'
 import { Link } from 'react-router-dom'
 
-const FavoritesCard = props => {
+// const FavoritesCard = props => {
+class FavoritesCard extends React.Component {
 
-    const handleClick = event => {
+    handleClick = event => {
         event.preventDefault()
-        props.fetchRecipe(props.favorite.attributes.recipe_identifier)
+        this.props.fetchRecipe(this.props.favorite.attributes.recipe_identifier)
     }
 
-    const handleDelete = event => {
+    handleDelete = event => {
         event.preventDefault()
-        props.deleteFavorite(props.favorite.id, props.history)
+        this.props.deleteFavorite(this.props.favorite.id, this.props.history)
     }
+    render() {
+        return (
+            <div className="favoritesCard">
+                <img src={this.props.favorite.attributes.recipe_image} alt="Food pic" className="favoritesCard-img"/><br/><br/>
+                <p>{this.props.favorite.attributes.recipe_title}</p><br/>
+                <button type="button" onClick={this.handleClick} className="favoritesCard-button"><Link className="favorites-linkbutton" to={`/recipe/${this.props.favorite.attributes.recipe_identifier}`}>View Recipe</Link></button><br/>
+                <button type="button" onClick={this.handleDelete} className="favoritesCard-button">Remove from Favorites</button>
+            </div>
+        )
 
-    return (
-        <div className="favoritesCard">
-            <img src={props.favorite.attributes.recipe_image} alt="Food pic" className="favoritesCard-img"/><br/><br/>
-            <p>{props.favorite.attributes.recipe_title}</p><br/>
-            <button type="button" onClick={handleClick} className="favoritesCard-button"><Link className="favorites-linkbutton" to={`/recipe/${props.favorite.attributes.recipe_identifier}`}>View Recipe</Link></button><br/>
-            <button type="button" onClick={handleDelete} className="favoritesCard-button">Remove from Favorite</button>
-        </div>
-    )
+    }
 }
 
 export default connect(null, { fetchRecipe, deleteFavorite })(FavoritesCard);
